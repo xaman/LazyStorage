ecf2a95997f4ad72298e6f16ab5b1526499b17dc
Martín Chamarro <martin.chamarro@gmail.com>
2016-06-12 20:13:42 +0200

    EDIT -Added some comments to explain how the methods of LazyStorage work

--- library/src/main/java/com/martinchamarro/lazystorage/LazyStorage.java
+++ library/src/main/java/com/martinchamarro/lazystorage/LazyStorage.java
@@ -9,6 +9,13 @@ import com.martinchamarro.lazystorage.utils.Logger;
 
 import java.util.List;
 
+/**
+ *
+ * This class is the point of access of the library.
+ * All the methods of the class work synchronously, so it's necessary to call them
+ * in background using a Thread, an AsyncTask or other thread implementation.
+ *
+ */
 public final class LazyStorage {
 
     private static final String TAG = LazyStorage.class.getSimpleName();
@@ -19,6 +26,12 @@ public final class LazyStorage {
         database = new DatabaseImpl(context);
     }
 
+    /**
+     * Saves an object to the database.
+     * Uses an IdProvider to get the object id by reflection.
+     *
+     * @param object to save
+     */
     public void save(Object object) {
         try {
             database.save(object);
@@ -27,6 +40,13 @@ public final class LazyStorage {
         }
     }
 
+    /**
+     * Saves an object to the database.
+     * Doesn't use reflection to get the id of the object.
+     *
+     * @param id of the object to save
+     * @param object to save in the database
+     */
     public void save(Object id, Object object) {
         try {
             database.save(id, object);
@@ -35,6 +55,12 @@ public final class LazyStorage {
         }
     }
 
+    /**
+     * Saves a list of objects to the database.
+     *
+     * @param objects to save
+     * @param <T>
+     */
     public <T> void saveAll(List<T> objects) {
         try {
             database.saveAll(objects);
@@ -43,6 +69,15 @@ public final class LazyStorage {
         }
     }
 
+    /**
+     * Loads an object from the database.
+     *
+     * @param id of the object to load
+     * @param classOfT of the object to load
+     * @param <T>
+     * @return the object to load or null if the object doesn't exist or
+     * there is some problem converting the saved data to an object.
+     */
     public <T> T load(Object id, Class<T> classOfT) {
         T result = null;
         try {
@@ -53,6 +88,14 @@ public final class LazyStorage {
         return result;
     }
 
+    /**
+     * Loads all the objects of the given class.
+     *
+     * @param classOfT is the class of the objects to load
+     * @param <T>
+     * @return a list of objects of the given class or an empty list if
+     * there aren't objects of this class or there is a problem loading them.
+     */
     public <T> List<T> loadAll(Class<T> classOfT) {
         List<T> result = null;
         try {
@@ -63,6 +106,13 @@ public final class LazyStorage {
         return result;
     }
 
+    /**
+     * Deletes from the database the object with the given id and the given class.
+     *
+     * @param id of the object to delete
+     * @param classOfT of the object to delete
+     * @param <T>
+     */
     public <T> void delete(Object id, Class<T> classOfT) {
         try {
             database.delete(id, classOfT);
@@ -71,6 +121,12 @@ public final class LazyStorage {
         }
     }
 
+    /**
+     * Deletes all the existing objects of the given class.
+     *
+     * @param classOfT of the all the objects to delete
+     * @param <T>
+     */
     public <T> void deleteAll(Class<T> classOfT) {
         try {
             database.deleteAll(classOfT);
@@ -79,6 +135,12 @@ public final class LazyStorage {
         }
     }
 
+    /**
+     * Deletes all the given objects from the database.
+     *
+     * @param objects is the list of objects to delete
+     * @param <T>
+     */
     public <T> void deleteAll(List<T> objects) {
         try {
             database.deleteAll(objects);
@@ -87,6 +149,9 @@ public final class LazyStorage {
         }
     }
 
+    /**
+     * Deletes all the content of the database.
+     */
     public void invalidate() {
         try {
             database.invalidate();
@@ -95,6 +160,9 @@ public final class LazyStorage {
         }
     }
 
+    /**
+     * Enables the log of the library that is disabled by default.
+     */
     public void enableLogging() {
         Logger.enable();
     }
